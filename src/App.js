67348f791f2efc32d483a4e8b6f353e543061e60
ygvit9fg <andmorebrawl@gmail.com>
2022-07-02 15:19:56 +0500

    load

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import ClassCounter from './components/classCounter';
 import Counter from './components/Counter';
-import PostItem from './components/PostItem';
+import PostItem from './components/PostItem.jsx';
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton'
 import MyInput from './components/UI/input/MyInput';
@@ -13,13 +13,36 @@ function App(){
       {id:2, title: 'Javascript', body: 'Description'},
       {id:3, title: 'Javascript', body: 'Description'},
     ])
+    const [title , setTitle] = useState('')
+    const [body , setBody] = useState('');
+
+    const addNewPost = (e) => {
+      e.preventDefault()
+      const newPost = {
+        id:Date.now().
+        title,
+        body
+      }
+      setPosts([...posts, newPost])
+     
+    }
 
 return(
   <div className='App'>
     <form>
-      <MyInput type="text" placeholder='Название поста'/>
-      <MyInput type="text" placeholder='Описание поста'/>
-      <MyButton>Создать пост</MyButton>
+      <MyInput
+       value={title}
+       onChahge={e => setTitle(e.target.value)}
+       type="text"
+       placeholder='Название поста'
+       />
+      <MyInput 
+      value={body}
+      onChahge={e => setBody(e.target.value)}
+      type="text" 
+      placeholder='Описание поста'
+      />
+      <MyButton onClick={addNewPost}>Создать пост</MyButton>
     </form>
     <PostList posts={posts} title="Посты про Javascipt"/>
   </div>
--- src/components/UI/button/MyButton.jsx
+++ src/components/UI/button/MyButton.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import classes from './MyButton.module.css';
-const MyButton = ({children, props}) =>{
+const MyButton = ({children, ...props}) =>{
     return(
-        <button className={classes.myBtn}>
-            {props.children}
+        <button {...props} className={classes.myBtn}>
+            {children}
 
         </button>
     )
